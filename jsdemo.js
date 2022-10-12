50137879d3f68f39318da92e7e0319b76196517b
shailesh-ss-19-11 <gokhaleshail.com>
2022-10-12 12:06:29 +0530

    11

--- jsdemo.js
+++ jsdemo.js
@@ -102,16 +102,137 @@
 
 // a<b || a>=b ? console.log("ha greater hai") : console.log("ha less hai") 
 
-const URL = "https://jsonplaceholder.typicode.com/posts"
-fetch(URL).then((response)=> response.json()).
-then((data)=> {
-    data.forEach(element => {
-        // console.log(element)
-        var title = element.title 
-        var description = element.body 
-        var id = element.userId 
-    });
-})
+// const name = "shailesh"     //define initialize and declare
+
+// string ////////////////////////////////////////////////////////////////////////////////////////////
+// collection of chars of data string is class
+
+// const first_name ="shailesh"
+// const mid_name = "manoj"
+// const last_name ="Gokhale"
+// let fullname =""
+// let len_of_name = name.length
+// console.log(len_of_name);
+
+// let nickname = name.slice(0,5)
+// console.log(nickname);
+
+// const splitted_name = name.split("l")
+// console.log(splitted_name[0]);           // it will return array 
+// console.log(splitted_name[1]);           // it will return array 
+
+
+// concatination  of string  /////////////////////////////////////////////////////////////////
+// fullname = first_name.concat(last_name)
+// console.log(fullname);
+
+// fullname = first_name+" "+mid_name+" "+last_name
+// console.log(fullname);
+// console.log(String.prototype)
+
+// capital or small 
+// const name = "shailesh"
+// console.log(name.toUpperCase())
+// console.log(name.toLowerCase())
+
+// let name = "shailesh"
+// console.log(name.length);
+// for(let i=0;i<name.length;i++)
+// {
+//     if(name[i]=="s"){
+//         console.log(i);
+//     }
+// }
+
+// Array ///////////////////////////////////////////////////////////////////////////////////////////////
+// array is collection of similar type of data  ------>  array is the collection of mixture of data
+
+let fruits = ["mango","apple","grapes","watermelon","lemon"];
+console.log(fruits)
+console.log(Array.isArray(fruits))  
+
+// console.log(fruits[0]);
+// console.log(fruits[1]);
+// console.log(fruits.slice(-1));
+
+// fruits.unshift("custoredApple");
+// console.log(fruits)
+
+// fruits.push("strawberry");
+// console.log(fruits)
+
+// popped_element= fruits.pop();
+// console.log(popped_element);
+// console.log(fruits)
+
+// shifted_element = fruits.shift();
+// console.log(shifted_element)
+
+for(let i=0 ; i< fruits.length ;i++){
+    // console.log(fruits[i])
+    if(fruits[i]=="apple"){
+        console.log(`${fruits[i]} color is red and position of ${fruits[i]} in array is ${i}`);
+        console.log(fruits[i]+" "+ "color is red and position of apple in array is "+""+ i );
+    }
+}
+
+
+
+
+
+
+
+
+
+// conditional statements 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const URL = "https://jsonplaceholder.typicode.com/posts"
+// fetch(URL).then((response)=> response.json()).
+// then((data)=> {
+//     data.forEach(element => {
+//         // console.log(element)
+//         var title = element.title 
+//         var description = element.body 
+//         var id = element.userId 
+//     });
+// })
 
 
 
